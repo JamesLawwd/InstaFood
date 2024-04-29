5dd9cff992db11230a80eb4a34d9ef2b59001cfb
james <jleo56291@gmail.com>
2024-04-29 13:09:47 +0300

    day 2 progress

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite here </h2>
+            <p> A food service technology solution that facilitates the seamless ordering process for customers and businesses</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
